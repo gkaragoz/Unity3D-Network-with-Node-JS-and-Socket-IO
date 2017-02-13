7421a3b3373ddd56a159d1f38d8a01675ccb5754
Gökhan Karagöz <annotto3@gmail.com>
2017-02-13 20:09:02 +0300

    Spawn existing players.

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,25 @@ var io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('Server started');
 
+var playerCount = 0;
+
 io.on('connection', function(socket){
 	console.log('Client connected, broadcasting spawn');
 	
 	socket.broadcast.emit('spawn');
-	
+	playerCount++;
+    
+    for(i=0; i<playerCount; i++){
+        socket.emit('spawn');
+        console.log('Sending spawn to new player');
+    }
+    
 	socket.on('move', function(data) {
 		console.log('Client moved');
 	});
+    
+    socket.on('disconnect', function() {
+        console.log('Client disconnected');
+        playerCount--;
+    });
 })
