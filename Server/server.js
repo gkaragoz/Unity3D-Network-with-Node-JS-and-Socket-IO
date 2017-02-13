80d10bcc2c0962330987f89721d199e526c86827
Gökhan Karagöz <annotto3@gmail.com>
2017-02-13 19:56:16 +0300

    Add spawn code for the first player.

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,9 @@ var io = require('socket.io')(process.env.PORT || 3000);
 console.log('Server started');
 
 io.on('connection', function(socket){
-	console.log('Client connected');
+	console.log('Client connected, broadcasting spawn');
 	
+	socket.broadcast.emit('spawn');
 	
 	socket.on('move', function(data) {
 		console.log('Client moved');
--- UnityNode/Assets/UnityNode/Scripts/Network.cs
+++ UnityNode/Assets/UnityNode/Scripts/Network.cs
@@ -6,14 +6,23 @@ public class Network : MonoBehaviour {
 
     static SocketIOComponent socket;
 
+    public GameObject playerPrefab;
+
 	void Start () {
         socket = GetComponent<SocketIOComponent>();
         socket.On("open", OnConnected);
+        socket.On("spawn", OnSpawned);
 	}
 
     void OnConnected(SocketIOEvent e)
     {
         Debug.Log("Connected");
         socket.Emit("move");
+    }
+
+    void OnSpawned(SocketIOEvent e)
+    {
+        Debug.Log("Player spawned");
+        Instantiate(playerPrefab);
     }
 }
